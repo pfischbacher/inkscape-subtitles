1f1aafb0d7823e45f309ed3ebddf21837be9d84c
Philip Fischbacher <pfischbacher@gmail.com>
2017-05-26 20:50:57 +0900

    initial commit

---
+++ make_subtitles.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+# A script to create subtitle svg files for Open Shot.
+
+create_subtitle() {
+    sed s/Textholder/"${2}"/ <dummy.svg>subtitle-$1.svg
+    inkscape \
+        --verb=EditSelectAll --verb=AlignHorizontalCenter \
+        --verb FileSave --verb=FileQuit subtitle-$1.svg
+}
+
+echo "Creating Subtitles for Open Shot."
+
+readarray -t Lines < lines.txt
+
+for i in "${!Lines[@]}"
+do
+    echo "i = $i, Line = ${Lines[$i]}"
+    create_subtitle $i "${Lines[$i]}"
+done
+
+echo "Finished."
+
